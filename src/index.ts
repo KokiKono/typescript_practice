b27e0441deb2e6115c926b6953c7def9d3cfa316
ohteru <arai_daiki@cam-inc.co.jp>
2019-09-04 10:50:23 +0900

    インデントをタブ4→スペース2

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,41 @@ import AutoPager from './autoPager/index';
 import Accordion from './accordion/index';
 
 window.addEventListener('DOMContentLoaded', () => {
-	// accordion
-	interface accordion {
-		components: NodeList;
-		triggerClass: string;
-		targetClass: string;
-	}
+  // accordion
+  interface accordion {
+    components: NodeList;
+    triggerClass: string;
+    targetClass: string;
+  }
 
-	const defaultComponents = document.querySelectorAll('.js-accordion');
-	const props: accordion = {
-		components: defaultComponents,
-		triggerClass: 'js-accordionTrigger',
-		targetClass: 'js-accordionTarget',
-	}
+  const defaultComponents = document.querySelectorAll('.js-accordion');
+  const props: accordion = {
+    components: defaultComponents,
+    triggerClass: 'js-accordionTrigger',
+    targetClass: 'js-accordionTarget',
+  }
 
-	const accordion: Accordion = new Accordion({ props });
-	accordion.atache();
+  const accordion: Accordion = new Accordion({ props });
+  accordion.atache();
 
 
-	const vending = new Vending();
-	// vending.vending();
+  const vending = new Vending();
+  // vending.vending();
 
-	interface autoPager {
-		target: Object,
-		moduleName: String,
-		prevName: String,
-		nextName: String,
-	};
+  interface autoPager {
+    target: Object,
+    moduleName: String,
+    prevName: String,
+    nextName: String,
+  };
 
-	const prop: autoPager = {
-		target: document.querySelectorAll('[data-modules]'),
-		moduleName: 'autoPager',
-		prevName: 'autoPagerPrev',
-		nextName: 'autoPagerNext',
-	};
+  const prop: autoPager = {
+    target: document.querySelectorAll('[data-modules]'),
+    moduleName: 'autoPager',
+    prevName: 'autoPagerPrev',
+    nextName: 'autoPagerNext',
+  };
 
-	const autoPager: AutoPager = new AutoPager({ prop });
-	autoPager.atache();
+  const autoPager: AutoPager = new AutoPager({ prop });
+  autoPager.atache();
 })
