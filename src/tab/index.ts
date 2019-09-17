1ff78b29f476268f1f02068a9fa69fc89fb35c8a
ohteru <arai_daiki@cam-inc.co.jp>
2019-09-17 09:03:11 +0900

    Add: custom event を使った tabの実装

--- src/tab/index.ts
+++ src/tab/index.ts
@@ -1,6 +1,5 @@
 interface tab {
   atache(): void;
-  // __update(): void;
 }
 
 interface HTMLElementEvent<T extends HTMLElement> extends Event {
@@ -10,7 +9,7 @@ interface HTMLElementEvent<T extends HTMLElement> extends Event {
 export default class Tab implements tab {
   head: NodeList;
   contents: NodeList;
-  componetClass: string = 'componentClass';
+  componetClass: string = 'js-tab';
 
   constructor (tabHead: NodeList, tabContents: NodeList) {
     this.head = tabHead;
@@ -20,31 +19,42 @@ export default class Tab implements tab {
   atache () {
     const tabs = document.querySelectorAll(`.${this.componetClass}`);
     tabs.forEach(item => {
-      item.addEventListener('tab-change', event => {
+      item.addEventListener('tab-change', (event: CustomEvent) => {
         event.stopPropagation();
-        console.log(event.type)
         this.__update({
           tabs,
-          // tabIndex: event.detail.index,
+          index: event.detail.index,
         });
       })
     });
 
-    this.head.forEach((head: Node, index: Number) => {
+    this.head.forEach((head: Node, index: number) => {
+      const tabIndex = String(index);
       head.addEventListener('click', (event: HTMLElementEvent<HTMLElement>) => {
-
         event.currentTarget.dispatchEvent(
-          this.__tabChangeEvent(event, index)
+          this.__tabChangeEvent(event, tabIndex)
         );
       })
     })
   }
 
-  __update ({}) {
+  __update (prop: {tabs, index: string}) {
+    const itemActiveClassName = 'Tab__headItem--active';
+    const contentsActiveClassName = 'Tab__contentsItem--active';
 
+    this.head.forEach((item: HTMLElement) => {
+      item.classList.remove(itemActiveClassName);
+    });
+
+    this.contents.forEach((item: HTMLElement) => {
+      item.classList.remove(contentsActiveClassName);
+    });
+
+    this.head[prop.index].classList.add(itemActiveClassName);
+    this.contents[prop.index].classList.add(contentsActiveClassName);
   }
 
-  __tabChangeEvent (event: HTMLElementEvent<HTMLElement>, index: Number): CustomEvent {
+  __tabChangeEvent (event: HTMLElementEvent<HTMLElement>, index: string): CustomEvent {
     return new CustomEvent('tab-change', {
       detail: {
         index: index,
